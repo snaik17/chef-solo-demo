0826799558f25e99ba8c799c36505781691f92e9
snaik17 <smith_naik@in.ibm.com>
2014-05-28 23:22:29 +0530

    Update default.rb

--- cookbooks/tomcat7/attributes/default.rb
+++ cookbooks/tomcat7/attributes/default.rb
@@ -1,13 +1,13 @@
 tc7target = "/usr/local"
 ##
-set[:tomcat7][:version] = "7.0.33"
+set[:tomcat7][:version] = "7.0.54"
 set[:tomcat7][:user] = "tomcat"
 set[:tomcat7][:group] = "tomcat"
 set[:tomcat7][:target] = "/usr/local"
 ##
-set[:tomcat7][:port] = 8080
-set[:tomcat7][:ssl_port] = 8443
-set[:tomcat7][:ajp_port] = 8009
+set[:tomcat7][:port] = 8087
+set[:tomcat7][:ssl_port] = 8447
+set[:tomcat7][:ajp_port] = 8007
 set[:tomcat7][:java_options] = " -Xmx128M -Dajva.awt.headless=true"
 set[:tomcat7][:use_security_manager] = "no"
 ##
